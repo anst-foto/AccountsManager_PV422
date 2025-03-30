2099105079f98e2ae00fac58d33741aa98ab6d19
Starinin Andrey <starinin-andrey@ya.ru>
2025-03-30 21:12:17 +0300

    Создание метода (и теста для него) для получения всех записей из БД

--- AccountsManager.Core.Test/ServiceTest.cs
+++ AccountsManager.Core.Test/ServiceTest.cs
@@ -19,5 +19,26 @@ namespace AccountsManager.Core.Test
             //Assert.Equal(result, true);
             Assert.True(result);
         }
+
+        [Fact]
+        public void GetAllAccountsTest()
+        {
+            var expected = new List<Account>()
+            {
+                new Account()
+                {
+                    Id = 1,
+                    LastName = "Starinin",
+                    FirstName = "Andrey",
+                    Login = "admin",
+                    Password = "123"
+                }
+            };
+            
+            var service = new Service();
+            var actual = service.GetAllAccounts();
+            
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- AccountsManager.Core/Service.cs
+++ AccountsManager.Core/Service.cs
@@ -1,18 +1,22 @@
-﻿using AccountsManager.Models;
+﻿using System.Data;
+using AccountsManager.Models;
 using Npgsql;
 
 namespace AccountsManager.Core;
 
 public class Service
 {
-    private const string CONNECTION_STRING = "Server=127.0.0.1;Port=5432;Database=accounts_db;User Id=postgres;Password=1234;";
+    private const string CONNECTION_STRING = "Server=127.0.0.1;Port=5432;Database=accounts_db;User Id=postgres;Password=1234;Search Path=test;";
     
     public bool AddAccount(Account account)
     {
         using var db = new NpgsqlConnection(CONNECTION_STRING);
         db.Open();
-        var sql = "INSERT INTO table_accounts(last_name, first_name, login, password) VALUES(@LastName, @FirstName, @Login, @Password)";
-        var command = new NpgsqlCommand(sql, db);
+        const string SQL = """
+                           INSERT INTO table_accounts(last_name, first_name, login, password) 
+                           VALUES(@LastName, @FirstName, @Login, @Password)
+                           """;
+        var command = new NpgsqlCommand(SQL, db);
         command.Parameters.AddWithValue("@LastName", account.LastName);
         command.Parameters.AddWithValue("@FirstName", account.FirstName);
         command.Parameters.AddWithValue("@Login", account.Login);
@@ -22,5 +26,42 @@ public class Service
         db.Close();
         
         return result > 0;
+    }
+
+    public IEnumerable<Account> GetAllAccounts()
+    {
+        using var db = new NpgsqlConnection(CONNECTION_STRING);
+        db.Open();
+        const string SQL = "SELECT id, last_name, first_name, login, password FROM table_accounts";
+        var command = new NpgsqlCommand(SQL, db);
+        var reader = command.ExecuteReader();
+        
+        if (!reader.HasRows) throw new Exception("Пустая таблица");
+
+        /*var accounts = new List<Account>();
+        while (reader.Read())
+        {
+            accounts.Add(new Account()
+            {
+                Id = reader.GetInt32("id"),
+                LastName = reader.GetString("last_name"),
+                FirstName = reader.GetString("first_name"),
+                Login = reader.GetString("login"),
+                Password = reader.GetString("password")
+            });
+        }
+        return accounts;*/
+
+        while (reader.Read())
+        {
+            yield return new Account()
+            {
+                Id = reader.GetInt32("id"),
+                LastName = reader.GetString("last_name"),
+                FirstName = reader.GetString("first_name"),
+                Login = reader.GetString("login"),
+                Password = reader.GetString("password")
+            };
+        }
     }
 }

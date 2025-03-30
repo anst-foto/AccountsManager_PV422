a4fd0bc0a223167cd3c19d344ce7c651896dfbfd
Starinin Andrey <starinin-andrey@ya.ru>
2025-03-30 20:52:31 +0300

    Добавление тестов

---
+++ AccountsManager.Core.Test/ServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using AccountsManager.Models;
+
+namespace AccountsManager.Core.Test
+{
+    public class ServiceTest
+    {
+        [Fact]
+        public void AddAccountTest()
+        {
+            var service = new Service();
+            var result = service.AddAccount(new Account()
+            {
+                LastName = "Starinin",
+                FirstName = "Andrey",
+                Login = "admin",
+                Password = "123"
+            });
+            
+            //Assert.Equal(result, true);
+            Assert.True(result);
+        }
+    }
+}
